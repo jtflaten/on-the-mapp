e8f64cf688500116d2f5247e2c7527fee17f280a
jtflaten <jtflaten@gmail.com>
2017-03-20 17:43:23 -0500

    Initial Commit

---
+++ on the mApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  on the mApp
+//
+//  Created by Jake Flaten on 3/20/17.
+//  Copyright © 2017 Break List. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
